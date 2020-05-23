49b92a7712628382b18aa11234eb903f4e9f1590
Anish Dutta <60481056+anishdutta@users.noreply.github.com>
2020-05-23 22:10:56 +0530

    graph

--- res/js/Charts.js
+++ res/js/Charts.js
@@ -21,6 +21,10 @@ var ctxL = document.getElementById("lineChart").getContext('2d');var ctxL = docu
           }
                   ]
               },
+              options: {
+                responsive: true,
+                maintainAspectRatio : false,
+              }
        
           });
         
@@ -30,22 +34,40 @@ var ctxL = document.getElementById("lineChart").getContext('2d');var ctxL = docu
         gradientFill.addColorStop(0, "rgba(230, 130, 13, 1)");
           gradientFill.addColorStop(1, "rgba(230, 130, 13, 0.1)");
 
-        var myLineChart = new Chart(ctxL, {
-          type: 'bar',
-          data: {
-          labels: ["01 march", "07 march", "14 March", "19 march", " 22 march", "25 march", "31 march"],
-          datasets: [
-          {
-              label: "Total Sales",
-              data: [50, 65, 45, 65, 35, 65, 0],
-              backgroundColor: gradientFill,
-              borderColor: [
-              '#EF820D',
-              ],
-              borderWidth: 2,
-              pointBackgroundColor: "orange",
-          }
-                  ]
-              },
-       
+          var myChart = new Chart(ctxL, {
+            type: 'bar',
+            data: {
+              labels: ["M", "T", "W", "T", "F", "S", "S"],
+              datasets: [{
+                label: 'apples',
+                data: [12, 19, 3, 17, 28, 24, 7],
+                backgroundColor: "rgba(153,255,51,1)"
+              }, {
+                label: 'oranges',
+                data: [30, 29, 5, 5, 20, 3, 10],
+                backgroundColor: "rgba(255,153,0,1)"
+              }]
+            }
           });
+        
+          // var myLineChart = new Chart(ctxL, {
+        //   type: 'bar',
+        //   data: {
+        //   labels: ["01 march", "07 march", "14 March", "19 march", " 22 march", "25 march", "31 march"],
+        //   datasets: [
+        //   {
+        //       label: "Total Sales",
+        //       data: [50, 65, 45, 65, 35, 65, 0],
+        //       backgroundColor: gradientFill,
+        //       borderColor: [
+        //       '#EF820D',
+        //       ],
+        //       borderWidth: 2,
+        //       pointBackgroundColor: "orange",
+        //   }
+        //           ]
+        //       },
+       
+        //   });
+
+        //   document.getElementById("myChart").getContext('2d');
